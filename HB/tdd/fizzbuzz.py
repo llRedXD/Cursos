594c21636fe424c54bb0a81ec079774429f94715
llRedXD <gh.o.neira@hotmail.com>
2022-07-26 14:53:21 -0300

    test(tdd): :white_check_mark: testes do fizzbuzz
    todos os teste foram aprovados

--- HB/tdd/fizzbuzz.py
+++ HB/tdd/fizzbuzz.py
@@ -7,7 +7,6 @@ se não for, imprima o número
 
 from functools import partial
 
-
 def robot(n):
     say = str(n)
     if multiple_of_3(n) and multiple_of_5(n):
@@ -19,25 +18,19 @@ def robot(n):
     return say
 
 
-multiple_of = lambda base, n: n % base == 0
+def multiple_of(base, n): return n % base == 0
+
+
 multiple_of_5 = partial(multiple_of, 5)
 multiple_of_3 = partial(multiple_of, 3)
 
 
+def assert_equal(result, expected):
+    from sys import _getframe
+    if result != expected:
+        current = _getframe().f_back
+        line_no = current.f_lineno
+        msg = "Fail: Line {} got {} expecting {}"
+        print(msg.format(line_no, result, expected))
 
-if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
 
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
-
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
-
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
