d7c6a36d3c7ce916d64b1376a7ec2761900b7d6c
llRedXD <gh.o.neira@hotmail.com>
2022-12-08 17:07:01 -0300

    feat(Aprenda Go): :sparkles: Cap.15 Finalizado e .gitignore adicionado

---
+++ aprendaGo/Cap15/Ex1/Ex1.go
@@ -0,0 +1,13 @@
+// - Crie um valor e atribua-o a uma variável.
+// - Demonstre o endereço deste valor na memória.
+package main
+
+import "fmt"
+
+func main() {
+
+    x := 10
+
+    fmt.Println(&x)
+
+}
